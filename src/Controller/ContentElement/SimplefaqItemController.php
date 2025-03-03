cc372568dbae9e63245e8229a8694a520400380c
Hamid Peywasti <hamid@respinar.com>
2025-03-03 12:52:40 +0330

    add SimplefaqIem content element

---
+++ src/Controller/ContentElement/SimplefaqItemController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Respinar\ContaoSimplefaqBundle\Controller\ContentElement;
+
+use Contao\ContentModel;
+use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
+use Contao\CoreBundle\Twig\FragmentTemplate;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+#[AsContentElement(category: 'simplefaq', template:'simplefaq_item')]
+class SimplefaqItemController extends AbstractContentElementController
+{
+
+    public const TYPE = 'simplefaq_item';
+
+    protected function getResponse(FragmentTemplate $template, ContentModel $model, Request $request): Response
+    {
+
+        // Store the FAQ item in a session or temporary storage
+        if (!isset($GLOBALS['FAQ_ITEMS'])) {
+            $GLOBALS['FAQ_ITEMS'] = [];
+        }
+
+        $GLOBALS['FAQ_ITEMS'][] = [
+            "@type" => "Question",
+            "name" => $model->simplefaq_question,
+            "acceptedAnswer" => [
+                "@type" => "Answer",
+                "text" => $model->simplefaq_answer
+            ]
+        ];
+
+        $template->set('question', $model->simplefaq_question);
+        $template->set('answer', $model->simplefaq_answer);
+
+        return $template->getResponse();
+    }
+}
